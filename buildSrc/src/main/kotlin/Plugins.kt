8bd77fc0f276ae3c45158a94d6225fc9f95e6c0c
Yunus Çağlıyan <yunuscagliyan97@gmail.com>
2023-09-25 10:15:09 +0300

    Set up Firebase.

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -3,4 +3,6 @@ object Plugins {
     const val androidKotlin = "org.jetbrains.kotlin.android"
     const val kotlinKapt = "kotlin-kapt"
     const val hilt = "com.google.dagger.hilt.android"
+    const val googleServices = "com.google.gms.google-services"
+    const val crashlytics = "com.google.firebase.crashlytics"
 }
