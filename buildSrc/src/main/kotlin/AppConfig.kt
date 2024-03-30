fe07e343e1b2c2d09393e37a9de5dc4ee5223f68
Yunus Çağlıyan <yunuscagliyan97@gmail.com>
2024-03-30 15:39:47 +0300

    1.1 version released.

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdk = 34
     const val minSdk = 24
     const val targetSdk = 34
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
     const val applicationId = "com.yunuscagliyan.wallx"
     const val jvmTarget = "17"
     const val kotlinCompilerExtensionVersion = "1.4.3"
